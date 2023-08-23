6023d7c77750a0060fe7185208ce94c8e6725d50
vikyw89 <vikyw89@gmail.com>
2023-08-23 12:41:54 +0800

    build: added dotenv, checking for server

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express from "express"
+const app = express()
+
+const PORT = process.env.PORT ?? 3001
+
+app.get('/', (req, res) => {
+    res.send(    res.json(`Server running on port ${PORT}`))
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
