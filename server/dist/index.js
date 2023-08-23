3cd3d30162315103e6da781c4ed34347ed906e4b
vikyw89 <vikyw89@gmail.com>
2023-08-23 14:40:05 +0800

    ci: adjusted husky script to only fix not to check

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 app.get("/", (req, res) => {
-  res.send(res.json(`Server running on port ${PORT}`));
+    res.send(res.json(`Server running on port ${PORT}`));
 });
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
 });
